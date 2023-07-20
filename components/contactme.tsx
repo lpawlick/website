1a090319cecb6484519f324d487aa0d426214ee8
lpawlick <122663606+lpawlick@users.noreply.github.com>
2023-07-20 10:29:34 +0200

    Made the contact option clickable and open the right program automatically

--- components/contactme.tsx
+++ components/contactme.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CryptoJS from "crypto-js";
-
+// @ts-ignore
+import Obfuscate from 'react-obfuscate';
+import {Link} from "@nextui-org/react";
 const secretKey = "$@!v4pqEXV4xkaxW9yGn$U5xDCZ!Q5BxZ$tVP!2!H6G3j9dm&3B@XQ4y2iiv56GD"; // Replace this with your secret key
 
 export const ContactMeHref = () => 
@@ -21,9 +23,15 @@ export const ContactMeHref = () =>
   const encryptedEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? "U2FsdGVkX18AfX3T8GT1VaKJEJH6TPVUwtDGgpESMZVPBuMNgBxlHII2IXlAWMNaKcrCcMUM3RNDaoChwYB67XYs9TYL6AcdnJnQgFri4eY82wsHiJS+BlTYrY/pCGBGcrZtWx9bx3JteUt89inFKC5tWs17z4rWLeaeM3Sc0M8XNbwAQe3OE7k1o4CEZdl8";
 
   return (
-    <div>
-      {decryptData(encryptedEmail)}
-    </div>
+    <div className="flex flex-row flex-wrap">
+    {encryptedEmail !== "U2FsdGVkX18AfX3T8GT1VaKJEJH6TPVUwtDGgpESMZVPBuMNgBxlHII2IXlAWMNaKcrCcMUM3RNDaoChwYB67XYs9TYL6AcdnJnQgFri4eY82wsHiJS+BlTYrY/pCGBGcrZtWx9bx3JteUt89inFKC5tWs17z4rWLeaeM3Sc0M8XNbwAQe3OE7k1o4CEZdl8" ? (
+      <Link isExternal showAnchorIcon>
+        <Obfuscate email={decryptData(encryptedEmail)} />
+      </Link>
+    ) : (
+      <span>{decryptData(encryptedEmail)}</span>
+    )}
+  </div>
   );
 };
 
